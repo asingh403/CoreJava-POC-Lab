537c39f03eeaf01b666a80faadebe1896439a179
Ashutosh Singh <asingh.it@hotmail.com>
2023-12-12 18:05:05 +0530

    Update FileOperation.java

--- src/main/java/com/poc/FileOperation.java
+++ src/main/java/com/poc/FileOperation.java
@@ -5,29 +5,33 @@ import java.util.Random;
 public class FileOperation {
 
     public static void main(String[] args) throws IOException {
-        String sourcePath = "src/main/java/resources/originalFile.pm"; // original file path
-        String destinationPath = "src/main/java/basefile/copiedFile.pm"; // new file path
+        String sourcePath = "src/main/java/resources/BDD.BACS18.ABC00012.PM"; // Example original file path
+        String destinationPath = "src/main/java/basefile/"; // Destination directory
 
         // Step 1: Copy the file
-        copyFile(sourcePath, destinationPath);
+        String copiedFilePath = copyFile(sourcePath, destinationPath);
 
-        // Step 2: Rename the file
-        String renamedFilePath = renameFile(destinationPath);
+        // Step 2: Rename the file with random digits
+        String renamedFilePath = renameFile(copiedFilePath);
 
         // Step 3: Update the file content
         updateFileContent(renamedFilePath);
     }
 
-    private static void copyFile(String sourcePath, String destinationPath) throws IOException {
-        Files.copy(Paths.get(sourcePath), Paths.get(destinationPath), StandardCopyOption.REPLACE_EXISTING);
+    private static String copyFile(String sourcePath, String destinationDir) throws IOException {
+        Path source = Paths.get(sourcePath);
+        String fileName = source.getFileName().toString();
+        Path destination = Paths.get(destinationDir + fileName);
+        Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);
+        return destination.toString();
     }
 
     private static String renameFile(String filePath) {
-        String newName = filePath.replace("copiedFile", "updatedFile"); // Create a new name
+        String newName = filePath.replaceAll("ABC\\d{5}", "ABC" + getRandomFiveDigits()); // Replaces the last 5 digits
         File file = new File(filePath);
         File renamedFile = new File(newName);
         if (file.renameTo(renamedFile)) {
-            System.out.println("File renamed successfully");
+            System.out.println("File renamed successfully to: " + newName);
         } else {
             System.out.println("Failed to rename file");
         }
