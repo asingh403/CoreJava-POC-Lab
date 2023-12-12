a73d3d47723ef8d045283b463385ada288f96124
Ashutosh Singh <asingh.it@hotmail.com>
2023-12-12 19:22:56 +0530

    Update S3UploaderUpdated.java

--- src/main/java/com/poc/S3UploaderUpdated.java
+++ src/main/java/com/poc/S3UploaderUpdated.java
@@ -1,61 +1,37 @@
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.RegionUtils;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.*;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 
 import java.io.File;
 
 public class S3UploaderUpdated {
 
-    private final AmazonS3 s3Client;
+    public static void main(String[] args) {
+        String accessKeyId = "BDD"; // Replace with your access key
+        String secretAccessKey = "Uhjdhhdshhds7w989we,jsdjdsjhdsjhds"; // Replace with your secret key
+        String bucketName = "TestSit"; // Replace with your S3 bucket name
+        String localFilePath = "/path/to/your/local/file.txt"; // Replace with your local file path
+        String s3Key = "path/in/bucket/filename.txt"; // Replace with your desired S3 key (path)
 
-    public S3Uploader(String accessKeyId, String secretAccessKey) {
         BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKeyId, secretAccessKey);
-        this.s3Client = AmazonS3ClientBuilder.standard()
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("https://object.ib4.jpmc.net", null))
+                .withPathStyleAccessEnabled(true)
                 .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
-                .withRegion(RegionUtils.getRegion().getName()) // Automatically determine the region
                 .build();
-    }
 
-    public String uploadFile(String bucketName, String key, String filePath) {
         try {
-            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key, new File(filePath));
-            s3Client.putObject(putObjectRequest);
-            System.out.println("File uploaded to S3: " + key);
+            // Upload the file
+            s3Client.putObject(new PutObjectRequest(bucketName, s3Key, new File(localFilePath)));
+            System.out.println("File uploaded to S3: " + s3Key);
 
             // Construct the S3 file URL
-            String fileUrl = String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, s3Client.getRegionName(), key);
-            return fileUrl;
+            String fileUrl = String.format("https://%s.s3.amazonaws.com/%s", bucketName, s3Key);
+            System.out.println("Uploaded file URL: " + fileUrl);
         } catch (Exception e) {
-            System.err.println(e.getMessage());
-            throw e;
-        }
-    }
-
-    public void deleteExistingFiles(String bucketName, String prefix) {
-        try {
-            ListObjectsRequest listObjectsRequest = new ListObjectsRequest().withBucketName(bucketName).withPrefix(prefix);
-            ObjectListing objectListing = s3Client.listObjects(listObjectsRequest);
-
-            while (true) {
-                for (S3ObjectSummary summary : objectListing.getObjectSummaries()) {
-                    s3Client.deleteObject(bucketName, summary.getKey());
-                }
-
-                // Check if the list is truncated, and get the next batch of objects
-                if (objectListing.isTruncated()) {
-                    objectListing = s3Client.listNextBatchOfObjects(objectListing);
-                } else {
-                    break;
-                }
-            }
-
-            System.out.println("Deleted existing files in S3 bucket: " + bucketName);
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-            throw e;
+            e.printStackTrace();
         }
     }
 }
