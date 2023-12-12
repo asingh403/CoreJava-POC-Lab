e5a4e5073c076a8fb6fa69fc0c6b0ab974206862
Ashutosh Singh <asingh.it@hotmail.com>
2023-12-12 19:44:43 +0530

    Create S3FileUploaderWithDel.java

---
+++ src/main/java/com/poc/S3FileUploaderWithDel.java
@@ -0,0 +1,60 @@
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+import java.io.File;
+
+public class FileUploader {
+
+    public static void main(String[] args) {
+        String accessKeyId = "BDD"; // Replace with your access key
+        String secretAccessKey = "Uhjdhhdshhds7w989we,jsdjdsjhdsjhds"; // Replace with your secret key
+        String bucketName = "TestSit"; // Replace with your S3 bucket name
+        String localFilePath = "/path/to/your/local/file.txt"; // Replace with your local file path
+        String s3Key = "path/in/bucket/filename.txt"; // Replace with your desired S3 key (path)
+
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKeyId, secretAccessKey);
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("https://object.ib4.jpmc.net", null))
+                .withPathStyleAccessEnabled(true)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                .build();
+
+        try {
+            // Delete existing files at the specified S3 key
+            deleteExistingFiles(s3Client, bucketName, s3Key);
+
+            // Upload the file
+            s3Client.putObject(new PutObjectRequest(bucketName, s3Key, new File(localFilePath)));
+            System.out.println("File uploaded to S3: " + s3Key);
+
+            // Construct the S3 file URL
+            String fileUrl = String.format("https://%s.s3.amazonaws.com/%s", bucketName, s3Key);
+            System.out.println("Uploaded file URL: " + fileUrl);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void deleteExistingFiles(AmazonS3 s3Client, String bucketName, String s3Key) {
+        ListObjectsRequest listObjectsRequest = new ListObjectsRequest()
+                .withBucketName(bucketName)
+                .withPrefix(s3Key);
+
+        ObjectListing objectListing;
+        do {
+            objectListing = s3Client.listObjects(listObjectsRequest);
+            for (S3ObjectSummary summary : objectListing.getObjectSummaries()) {
+                s3Client.deleteObject(new DeleteObjectRequest(bucketName, summary.getKey()));
+                System.out.println("Deleted file: " + summary.getKey());
+            }
+            listObjectsRequest.setMarker(objectListing.getNextMarker());
+        } while (objectListing.isTruncated());
+    }
+}
